76719b54014089aff824b5cbe13dd82069e277f5
Alex <AlexCarterDeveloper@gmail.com>
2019-01-13 00:23:00 +0300

    Remove demo files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            Project list
+        </header>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+export default App;
