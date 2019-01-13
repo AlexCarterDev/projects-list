bfe06fc0e99a276965d87d23a6d7c16abceb170d
Alex <AlexCarterDeveloper@gmail.com>
2019-01-13 14:16:23 +0300

    Create components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './style.scss';
+import preview_description from './previews';
+
+class Preview extends Component {
+    render() {
+        return (
+            <div className="preview">
+                <img className='preview_img' alt={this.props.title} src={this.props.img}/>
+                <h4 className='preview_title'>{this.props.title}</h4>
+                <p className='preview_text'>{this.props.text}</p>
+                <p className='preview_bottom'>{this.props.bottom}</p>
+            </div>
+        )
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-            Project list
-        </header>
-      </div>
-    );
-  }
+    render() {
+
+        return (
+            <div id="app-container">
+                <h3>My Projects</h3>
+                {preview_description.map(d => 
+                    <Preview 
+                        key={d.id} 
+                        title={d.title} 
+                        text={d.text}
+                        bottom={d.bottom_text}
+                        img={d.img}
+                        github={d.github}
+                />)}
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
