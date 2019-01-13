50fd3b0ff4e18738c68c9cb23a4625764a8cc3b5
Alex <AlexCarterDeveloper@gmail.com>
2019-01-13 17:18:58 +0300

    Add apps description

--- src/previews.js
+++ src/previews.js
@@ -15,6 +15,54 @@ var preview_description = [
         link: "apps/markdown_previewer/index.html",
         github: "https://github.com/AlexCarterDev/my-frontend-snippets/tree/master/p03_markdown-previewer"
     },
+    {
+        title: "Drum Machine",
+        text: "Playing samples. Have a three bank with diffrent sets of sampels.",
+        bottom_text: "React, Redux",
+        img: "images/drum_machine.png",
+        link: "apps/drum_machine/index.html",
+        github: "https://github.com/AlexCarterDev/my-frontend-snippets/tree/master/p04_drum-machine"
+    },
+    {
+        title: "Javascript Calculator",
+        text: "Calculator with basic arithmetic.",
+        bottom_text: "React, Redux",
+        img: "images/calculator.png",
+        link: "apps/calculator/index.html",
+        github: "https://github.com/AlexCarterDev/my-frontend-snippets/tree/master/p05_javascript-calculator"
+    },
+    {
+        title: "Pomodoro Clock",
+        text: "Break down work into intervals, by default 25 minutes in length, separated by 5 mintes short breaks",
+        bottom_text: "React, Redux",
+        img: "images/pomodoro_clock.png",
+        link: "apps/pomodoro_clock/index.html",
+        github: "https://github.com/AlexCarterDev/my-frontend-snippets/tree/master/p06_pomodoro-clock"
+    },
+    {
+        title: "Todo List",
+        text: "Task manager implemented using Sortable library and local browser storage (web storage)",
+        bottom_text: "JavaScript, Sortable",
+        img: "images/todo_list.png",
+        link: "apps/todo_list/index.html",
+        github: "https://github.com/AlexCarterDev/my-frontend-snippets/tree/master/p01_todo-list"
+    },
+    // {
+    //     title: "Telegraph",
+    //     text: "Mimic style",
+    //     bottom_text: "HTML, CSS",
+    //     img: "images/telegraph.png",
+    //     link: "pages/telegraph/index.html",
+    //     github: "https://github.com/AlexCarterDev/learning_web_replacing/tree/master/p01%20-%20Telegraph.%20Jupiter"
+    // },
+    // {
+    //     title: "Fifty coffies",
+    //     text: "One page",
+    //     bottom_text: "HTML, CSS",
+    //     img: "images/fifty_coffees.png",
+    //     link: "pages/fifty_coffees/index.html",
+    //     github: "https://github.com/AlexCarterDev/learning_web_replacing/tree/master/p04%20-%20Fifty%20Coffees"
+    // },
 ];
 
 export default preview_description; 
